37e9696ce7e5063652b9728672d731c23ccd26cf
Hồ Sỹ Huy <huyhs@bizflycloud.vn>
2024-07-09 01:51:02 +0700

    feat: fastapi basic update

--- main.py
+++ main.py
@@ -1,103 +1,81 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,  Query, Request
+import uuid
+from datetime import datetime
 import json
 import uvicorn
-from pydantic import BaseModel
-from datetime import datetime
+from unidecode import unidecode
+import jsonlines
 
 app = FastAPI()
 
-def readJsonData(filePath):
-  with open(filePath, 'r') as file:
-    return json.load(file)
+def read_file(file_path):
+  with jsonlines.open(file_path) as reader:
+    file_data = []
+    for obj in reader:
+      # Process each JSON object (obj) as needed
+      file_data.append(obj)
+    return file_data
 
-def writeJsonData(filePath, data):
-  with open(filePath, 'w') as file:
-    json.dump(data, file, indent=2, sort_keys=True, default=str)
+def paginate(items, page, limit):
+    start_index = (page - 1) * limit
+    end_index = start_index + limit
+    return items[start_index:end_index]
 
-# Pydantic model for request body
-class Item(BaseModel):
-  name: str
-  symbol: str
-  price: int
-  marketcap: int
-  created_at: datetime = ''
-  updated_at: datetime = ''
+def write_file(file_path, obj, write_method):
+  with open(file_path, write_method) as file:
+    json_line = json.dumps(obj)
+    file.write(json_line + '\n')  
 
+# name: str = '', price: str = ''
+@app.get('/products')
+async def getItems(page: int = Query(default = 1),
+                   limit: int = Query(default = 1),
+                   name: str = '', job: str = ''):
+  file_data = read_file('data.jsonl')
+  if name or job:
+    filter_data = []
+    for row in file_data:
+      if name and job and (name in unidecode(row["name"].lower())) and (job in unidecode(row["job"].lower())):
+        filter_data.append(row)
+      elif name and (name in unidecode(row["name"].lower())):
+        filter_data.append(row)
+      elif job and (job in unidecode(row["job"].lower())):
+        filter_data.append(row)
+    return paginate(filter_data, page, limit)
+  return paginate(file_data, page, limit)
 
-    # get all items
-@app.get("/items")
-async def getAllItem():
-  data = readJsonData('import.json')
-  return data
+@app.post('/product')
+async def createProduct(
+    name: str, 
+    age: int, 
+    job: str = ''):
+  print("name:" + name)
+  print("age:" + age)
+  print("job:" + job)
+  payload = {
+    "id": str(uuid.uuid4()),
+    "name": name,
+    "age": age,
+    "job": job,
+    "created_at": str(datetime.now()),
+    "updated_at": str(datetime.now()),
+  }
+  write_file('data.jsonl', payload, 'a')
+  return payload
 
+@app.delete('/product/{product_id}')
+async def deleteProduct(product_id: str):
+  file_data = read_file('data.jsonl')
+  findProduct = [product for product in file_data if product["id"] == product_id]
+      
+  if findProduct:
+    for product in file_data:
+      if product["id"] != product_id:
+        write_file('data.jsonl', product, 'a')
+    return "Delete product success!"
+  else:
+    return "Product not found!"
 
-# get a item by id
-@app.get("/item/{id}")
-async def getItemByID(id: int):
-  data = readJsonData('import.json')
-  findItem = {}
-  for item in data:
-    if id == item["id"]:
-      findItem = item
-
-  if len(findItem) == 0:
-    return "Item not found!"
-  return findItem
-
-
-# create a item
-@app.post("/item")
-async def createItem(item: Item):
-  data = Item(
-    name = item.name, 
-    symbol = item.symbol, 
-    price = item.price, 
-    marketcap = item.marketcap, 
-    created_at = datetime.now(),
-    updated_at = datetime.now()
-  )
-  writeJsonData('export.json', json.loads(data.model_dump_json()))
-  return data
-
-
-# update a item
-@app.put("/item/{id}")
-async def updateItem(item: Item, id: int):
-  data = readJsonData('import.json')
-  selectedData = {}
-  newArr = []
-  for el in data:
-    if id == el["id"]:
-      el = {
-        **el,
-        "name": item.name, 
-        "symbol": item.symbol, 
-        "price": item.price, 
-        "marketcap": item.marketcap, 
-        "updated_at": datetime.now(),
-      }
-      selectedData = el
-    newArr.append(el)
-  writeJsonData('import.json', newArr)
-
-  if len(selectedData) == 0:
-    return "Item not found!"
-  return selectedData
-
-
-# delete a item
-@app.delete("/item/{id}")
-async def deleteItem(id: int):
-  data = readJsonData('import.json')
-  selectedData = {}
-  for item in data:
-    if id == item["id"]:
-      selectedData = item
-      data.pop(data.index(item))
-  writeJsonData('import.json', data)
-  if len(selectedData) == 0:
-    return "Item not found!"
-  return data
 
 if __name__ == '__main__':
-  uvicorn.run('main:app', reload=True)
+  uvicorn.run()
